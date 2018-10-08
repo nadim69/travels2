5cb19b383b12067cca0156c890211f5a0fb512a3
Nadim Mabrouk <mabrouknadim2@gmail.com>
2018-10-08 10:13:38 +0200

    React3

--- src/App.js
+++ src/App.js
@@ -6,25 +6,33 @@ import Quote from "./Quote";
 import Lamp from "./lamp";
 
 class App extends Component {
-    constructor(){
-      super();
-      this.state ={
+
+    constructor(props){
+      super(props)
+      this.state = {
         working : true
-      }
-    };
- AtomeReverse = () => {
-    this.setState = { workig: !this.state.true}};
+
+           }
+    }
+  
+
+ handleClick = () => {
+    this.setState({working: !this.state.working})
+  }
 
   render() {
+
+    let change = this.state.working ? "App-logo": "App-logo-new";
 
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className={change} alt="logo" />
           <h1 className="App-title">My Travel</h1>
         </header>
-          <button onClick={this.AtomeReverse}
-          />
+          <br /><br />
+           <button onClick={this.handleClick}>CLICK
+           </button>
           <Lamp on />
           <Lamp />
          <Quote
@@ -33,7 +41,7 @@ class App extends Component {
           image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
         />
       </div>
-    );
+    )
   }
 }
 
